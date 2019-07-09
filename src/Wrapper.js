01b278bcc334de032b57495921c0e28931b7bb3f
SamdeVega <s.a.devega.garcia@gmail.com>
2019-07-09 06:25:56 +0100

    Should wrap a sentence when it does not fit

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,8 +1,8 @@
 const Wrapper = () => {
   const wrap = (string, columnNumber) => {
-    let wrapped = string.substring(0, columnNumber)
+    let wrapped = string.substring(0, columnNumber).trim()
     if (string.length > columnNumber)
-      wrapped = wrapped.concat("\n").concat(string.substring(columnNumber))
+      wrapped = wrapped.concat("\n").concat(string.substring(columnNumber).trim())
     return wrapped
   }
 
--- test/Wrapper.spec.js
+++ test/Wrapper.spec.js
@@ -11,4 +11,8 @@ describe('Wrapper should wrap', () => {
   it('a word when it does not fit', () => {
     expect(wrapper.wrap("hello", 3)).toBe("hel\nlo")
   })
+
+  it('a sentence when it does not fit', () => {
+    expect(wrapper.wrap("hello world!", 6)).toBe("hello\nworld!")
+  })
 })
